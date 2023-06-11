00623ae872096af48698c4dd4f583bc0d8eef256
Sbachra <bachrasoufiane@gmail.com>
2023-06-11 22:05:06 +0000

    Sara

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+mport sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+    argv = sys.argv[1:]
+
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[0]))
+    else:
+        print("{} arguments:".format(argc))
+        for i, arg in enumerate(argv):
+            print("{}: {}".format(i + 1, arg))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+mport sys
+
+if __name__ == "__main__":
+    result = 0
+    for arg in sys.argv[1:]:
+        result += int(arg)
+
+    print(result)
